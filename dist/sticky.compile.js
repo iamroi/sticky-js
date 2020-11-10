12bbe7816e3a94c585f9c266b46907eb9bf8ea50
Raja Munisamy <mrajasays@gmail.com>
2020-11-10 11:38:10 +1100

    update member profile and organisation forms

--- dist/sticky.compile.js
+++ dist/sticky.compile.js
@@ -38,6 +38,7 @@ var Sticky = /*#__PURE__*/function () {
       marginTop: options.marginTop || 0,
       marginBottom: options.marginBottom || 0,
       stickyFor: options.stickyFor || 0,
+      stickyDisableFor: options.stickyDisableFor || Infinity,
       stickyClass: options.stickyClass || null,
       stickyContainer: options.stickyContainer || 'body'
     };
@@ -88,6 +89,8 @@ var Sticky = /*#__PURE__*/function () {
       element.sticky.marginTop = parseInt(element.getAttribute('data-margin-top')) || this.options.marginTop;
       element.sticky.marginBottom = parseInt(element.getAttribute('data-margin-bottom')) || this.options.marginBottom;
       element.sticky.stickyFor = parseInt(element.getAttribute('data-sticky-for')) || this.options.stickyFor;
+      element.sticky.stickyDisableFor = parseInt(element.getAttribute('data-sticky-disable-for')) || this.options.stickyDisableFor;
+      element.sticky.stickyDisableFor = element.sticky.stickyDisableFor < 0 ? Infinity : element.sticky.stickyDisableFor;
       element.sticky.stickyClass = element.getAttribute('data-sticky-class') || this.options.stickyClass;
       element.sticky.wrap = element.hasAttribute('data-sticky-wrap') ? true : this.options.wrap; // @todo attribute for stickyContainer
       // element.sticky.stickyContainer = element.getAttribute('data-sticky-container') || this.options.stickyContainer;
@@ -131,7 +134,7 @@ var Sticky = /*#__PURE__*/function () {
   }, {
     key: "activate",
     value: function activate(element) {
-      if (element.sticky.rect.top + element.sticky.rect.height < element.sticky.container.rect.top + element.sticky.container.rect.height && element.sticky.stickyFor < this.vp.width && !element.sticky.active) {
+      if (element.sticky.rect.top + element.sticky.rect.height < element.sticky.container.rect.top + element.sticky.container.rect.height && element.sticky.stickyFor < this.vp.width && element.sticky.stickyDisableFor > this.vp.width && !element.sticky.active) {
         element.sticky.active = true;
       }
 
@@ -191,10 +194,11 @@ var Sticky = /*#__PURE__*/function () {
       this.vp = this.getViewportSize();
       element.sticky.rect = this.getRectangle(element);
       element.sticky.container.rect = this.getRectangle(element.sticky.container);
+      console.log(element.sticky.stickyDisableFor);
 
-      if (element.sticky.rect.top + element.sticky.rect.height < element.sticky.container.rect.top + element.sticky.container.rect.height && element.sticky.stickyFor < this.vp.width && !element.sticky.active) {
+      if (element.sticky.rect.top + element.sticky.rect.height < element.sticky.container.rect.top + element.sticky.container.rect.height && element.sticky.stickyFor < this.vp.width && element.sticky.stickyDisableFor > this.vp.width && !element.sticky.active) {
         element.sticky.active = true;
-      } else if (element.sticky.rect.top + element.sticky.rect.height >= element.sticky.container.rect.top + element.sticky.container.rect.height || element.sticky.stickyFor >= this.vp.width && element.sticky.active) {
+      } else if (element.sticky.rect.top + element.sticky.rect.height >= element.sticky.container.rect.top + element.sticky.container.rect.height || (element.sticky.stickyFor >= this.vp.width || element.sticky.stickyDisableFor <= this.vp.width) && element.sticky.active) {
         element.sticky.active = false;
       }
 
@@ -401,9 +405,11 @@ var Sticky = /*#__PURE__*/function () {
         width: '',
         top: '',
         left: ''
-      });
-      var width = Math.max(element.offsetWidth, element.clientWidth, element.scrollWidth);
-      var height = Math.max(element.offsetHeight, element.clientHeight, element.scrollHeight);
+      }); // const width = Math.max(element.offsetWidth, element.clientWidth, element.scrollWidth);
+      // const height = Math.max(element.offsetHeight, element.clientHeight, element.scrollHeight);
+
+      var width = Math.max(element.offsetWidth, element.clientWidth);
+      var height = Math.max(element.offsetHeight, element.clientHeight);
       var top = 0;
       var left = 0;
 
